85901c752b3efda64a90e8a462aeb2a2e384b8fa
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-21 21:14:24 +0300

    conditional statements

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Write your code here! 
+  if (feet > 2500)
+{
+  return `No can do.`
+  
+}
+else if (feet > 2000) {
+  return `I will gladly take your thirty bucks.`
+}
+else if (feet <= 400)
+{
+  return `This one is on me!`;
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+
+const whereTo = (city ==='NYC')? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
+  return whereTo;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let message;
+  switch (tip) {
+    case 'generous':
+      message= `Thank you so much.`;
+      break;
+    case 'not as generous':
+      message= `Thank you.`;
+      break;
+    default:
+      message= `Bye.`;
+      break;
+  }
+  return message;
 }
